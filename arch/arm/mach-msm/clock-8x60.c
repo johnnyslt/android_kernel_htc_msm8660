8a959882dafd29dcba54d9883c62944a7f8f7932
Stephen Boyd <sboyd@codeaurora.org>
2011-12-15 20:47:14 -0800

    msm: scm-pas: Request bandwidth for auth_and_reset()
    The secure processor doesn't have the capability to request
    bandwidth itself. Add bandwidth requests for it so that
    performance doesn't suffer.
    
    The bus driver is lacking a couple features:
    
     (1) No control of daytona fabric
    
     (2) No endpoints for the crypto engine on the daytona fabric
    
    Solve (1) with a clock voter on daytona fabric and (2) with a
    bandwidth request for SPS which lives on the daytona fabric.
    
    Change-Id: Ib6ca67043bc3b97dbbecb1b571c4d33721fae11c
    CRs-Fixed: 319454
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- arch/arm/mach-msm/clock-8x60.c
+++ arch/arm/mach-msm/clock-8x60.c
@@ -3210,6 +3210,7 @@ static DEFINE_CLK_VOTER(dfab_sdc2_clk, &dfab_clk.c);
 static DEFINE_CLK_VOTER(dfab_sdc3_clk, &dfab_clk.c);
 static DEFINE_CLK_VOTER(dfab_sdc4_clk, &dfab_clk.c);
 static DEFINE_CLK_VOTER(dfab_sdc5_clk, &dfab_clk.c);
+static DEFINE_CLK_VOTER(dfab_scm_clk, &dfab_clk.c);
 
 static DEFINE_CLK_VOTER(ebi1_msmbus_clk, &ebi1_clk.c);
 static DEFINE_CLK_VOTER(ebi1_adm0_clk,   &ebi1_clk.c);
@@ -3824,6 +3825,7 @@ static struct clk_lookup msm_clocks_8x60[] = {
 	CLK_LOOKUP("bus_clk",		dfab_sdc3_clk.c, "msm_sdcc.3"),
 	CLK_LOOKUP("bus_clk",		dfab_sdc4_clk.c, "msm_sdcc.4"),
 	CLK_LOOKUP("bus_clk",		dfab_sdc5_clk.c, "msm_sdcc.5"),
+	CLK_LOOKUP("bus_clk",		dfab_scm_clk.c,	"scm"),
 
 	CLK_LOOKUP("ebi1_msmbus_clk",	ebi1_msmbus_clk.c, NULL),
 	CLK_LOOKUP("mem_clk",		ebi1_adm0_clk.c, "msm_dmov.0"),
