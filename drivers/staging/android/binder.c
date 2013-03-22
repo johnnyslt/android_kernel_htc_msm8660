022a53444d96834346eeaa655db367a8ee62b659
mrg666 <drgungor@hotmail.com>
2013-03-22 18:39:28 -0400

    Staging: android: binder: Remove an inconsequential conditional macro
    Removes an inconsequential conditional macro.
    
    Cc: Arve Hjønnevåg <arve@android.com>
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -416,13 +416,11 @@ repeat:
 	else
 		FD_CLR(fd, fdt->close_on_exec);
 	files->next_fd = fd + 1;
-#if 1
 	/* Sanity check */
 	if (fdt->fd[fd] != NULL) {
 		printk(KERN_WARNING "get_unused_fd: slot %d not NULL!\n", fd);
 		fdt->fd[fd] = NULL;
 	}
-#endif
 	error = fd;
 
 out:
