a2e5950572e8ddbc8571a0db3642dbe4878f232c
Darescu Ionut <johnny@esi.ro>
2013-03-26 10:49:47 +0200

    undervolt wifi card

--- arch/arm/mach-msm/board-shooter_u-mmc.c
+++ arch/arm/mach-msm/board-shooter_u-mmc.c
@@ -180,7 +180,7 @@ static unsigned int shooter_u_wifi_status(struct device *dev)
 
 static unsigned int shooter_u_wifislot_type = MMC_TYPE_SDIO_WIFI;
 static struct mmc_platform_data shooter_u_wifi_data = {
-	.ocr_mask               = MMC_VDD_28_29,
+	.ocr_mask               = MMC_VDD_24_25,
 	.status                 = shooter_u_wifi_status,
 	.register_status_notify = shooter_u_wifi_status_register,
 	.embedded_sdio          = &shooter_u_wifi_emb_data,
