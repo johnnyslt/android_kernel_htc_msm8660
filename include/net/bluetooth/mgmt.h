5b9a81b7e330ad959560def2703929fc25c6cdde
Darescu Ionut <johnny@esi.ro>
2013-04-07 23:41:54 +0300

    Revert "Chad.G: BLUETOOTH: made it work with sense and aosp roms"
    This reverts commit 95970f4e17ebeaeb067debd787415218bac72fec.
    
    Conflicts:
    	net/bluetooth/sco.c

--- include/net/bluetooth/mgmt.h
+++ include/net/bluetooth/mgmt.h
@@ -161,6 +161,7 @@ struct mgmt_cp_set_io_capability {
 struct mgmt_cp_pair_device {
 	bdaddr_t bdaddr;
 	__u8 io_cap;
+	__u8 ssp_cap;
 } __packed;
 struct mgmt_rp_pair_device {
 	bdaddr_t bdaddr;
@@ -231,19 +232,6 @@ struct mgmt_cp_set_connection_params {
 struct mgmt_cp_encrypt_link {
 	bdaddr_t bdaddr;
 	__u8 enable;
-} __packed;
-
-#define MGMT_OP_SET_RSSI_REPORTER		0x0022
-struct mgmt_cp_set_rssi_reporter {
-	bdaddr_t	bdaddr;
-	__s8		rssi_threshold;
-	__le16	interval;
-	__u8		updateOnThreshExceed;
-} __packed;
-
-#define MGMT_OP_UNSET_RSSI_REPORTER		0x0023
-struct mgmt_cp_unset_rssi_reporter {
-	bdaddr_t	bdaddr;
 } __packed;
 
 #define MGMT_EV_CMD_COMPLETE		0x0001
@@ -372,10 +360,4 @@ struct mgmt_ev_remote_version {
 struct mgmt_ev_remote_features {
 	bdaddr_t bdaddr;
 	uint8_t features[8];
-} __packed;
-
-#define MGMT_EV_RSSI_UPDATE		0x0020
-struct mgmt_ev_rssi_update {
-	bdaddr_t	bdaddr;
-	__s8			rssi;
 } __packed;
