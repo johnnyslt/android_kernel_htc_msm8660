d8581fc079e06b56700ab6ec562a11fc48a8e791
Darescu Ionut <johnny@esi.ro>
2013-04-08 03:47:49 +0300

    Update baterry driver

--- arch/arm/mach-msm/include/mach/htc_battery_core.h
+++ arch/arm/mach-msm/include/mach/htc_battery_core.h
@@ -70,6 +70,7 @@ struct battery_info_reply {
 
 struct htc_battery_core {
 	int (*func_show_batt_attr)(struct device_attribute *attr, char *buf);
+        int (*func_show_cc_attr)(struct device_attribute *attr, char *buf);
 	int (*func_get_battery_info)(struct battery_info_reply *buffer);
 	int (*func_charger_control)(enum charger_control_flag);
 	int (*func_context_event_handler)(enum batt_context_event);
