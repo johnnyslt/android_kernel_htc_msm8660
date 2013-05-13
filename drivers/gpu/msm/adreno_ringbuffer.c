6daa2fd53778fe2c301c9c3b61502cd214cf899f
Darescu Ionut <johnny@esi.ro>
2013-05-13 10:16:43 +0300

    msm: kgsl: Turn off the CP_DEBUG dynamic clock
    The CP dynamic clock seems to be glitchy when the CP clocks are turned
    back on after a power event. Turn off said dynamic clock control at
    init time. The impact of leaving the dynamic clock control off is
    negligible since the CP clock is only on when the CP is actually in
    use.
    
    CRs-fixed: 402119
    CRs-fixed: 409253
    CRs-fixed: 413224
    Change-Id: Ic0dedbad783f8b911d9b57d1602d9b3976af1b3b
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>

--- drivers/gpu/msm/adreno_ringbuffer.c
+++ drivers/gpu/msm/adreno_ringbuffer.c
@@ -47,6 +47,14 @@
 #define A225_PFP_FW "a225_pfp.fw"
 #define A225_PM4_FW "a225_pm4.fw"
 
+/*
+ * CP DEBUG settings for all cores:
+ * DYNAMIC_CLK_DISABLE [27] - turn off the dynamic clock control
+ * PROG_END_PTR_ENABLE [25] - Allow 128 bit writes to the VBIF
+ */
+
+#define CP_DEBUG_DEFAULT ((1 << 27) | (1 << 25))
+
 static void adreno_ringbuffer_submit(struct adreno_ringbuffer *rb)
 {
 	BUG_ON(rb->wptr == 0);
