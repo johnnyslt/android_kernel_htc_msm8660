3d063ae6e20c4259ab6c2722e9e45196c1bd31bf
Darescu Ionut <johnny@esi.ro>
2013-05-24 21:45:12 +0300

    Add Dynamic read-ahead

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -1473,7 +1473,9 @@ void task_dirty_inc(struct task_struct *tsk);
 
 /* readahead.c */
 #define VM_MAX_READAHEAD	2048	/* kbytes */
-#define VM_MIN_READAHEAD	16	/* kbytes (includes current page) */
+#define VM_MIN_READAHEAD	64	/* kbytes (includes current page) */
+
+extern unsigned long max_readahead_pages;
 
 int force_page_cache_readahead(struct address_space *mapping, struct file *filp,
 			pgoff_t offset, unsigned long nr_to_read);
