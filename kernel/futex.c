e860931a6e2771f263c063b8f2a88a4b6f9bc093
Darescu Ionut <johnny@esi.ro>
2013-07-06 00:46:30 +0300

    cgroups: introduce timer slack controller
    v9:
     - update documentation
     - minor cleanup
    v8:
     - change hiearchy rules
     - introduce timer_slack.effective_slack_ns
     - get_task_timer_slack() -> task_get_effective_timer_slack()
     - task_get_effective_timer_slack() splited in separate patch
     - implement PR_GET_EFFECTIVE_TIMERSLACK
    v7:
     - totally reworked interface and rewritten from scratch
       (See Documentation/cgroups/timer_slack.txt for more information)
    v6:
     - add documentation
     - use notifier_call_chain() instead of check hook
     - fix validate_change()
     - cleanup
    v5:
     - -EBUSY on writing to timer_slack.min_slack_ns/max_slack_ns if a child has
       wider min-max range
    v4:
     - hierarchy support
     - drop dummy_timer_slack_check()
     - workaround lockdep false (?) positive
     - allow 0 as timer slack value
    v3:
     - rework interface
     - s/EXPORT_SYMBOL/EXPORT_SYMBOL_GPL/
    v2:
     - fixed with CONFIG_CGROUP_TIMER_SLACK=y
    v1:
     - initial revision
    
    Kirill A. Shutemov (3):
      hrtimer: introduce effective timer slack
      hrtimer: implement PR_GET_EFFECTIVE_TIMERSLACK
      cgroups: introduce timer slack controller

--- kernel/futex.c
+++ kernel/futex.c
@@ -1910,7 +1910,7 @@ static int futex_wait(u32 __user *uaddr, unsigned int flags, u32 val,
 				      HRTIMER_MODE_ABS);
 		hrtimer_init_sleeper(to, current);
 		hrtimer_set_expires_range_ns(&to->timer, *abs_time,
-					     current->timer_slack_ns);
+					     task_get_effective_timer_slack(current));
 	}
 
 retry:
@@ -2307,7 +2307,7 @@ static int futex_wait_requeue_pi(u32 __user *uaddr, unsigned int flags,
 				      HRTIMER_MODE_ABS);
 		hrtimer_init_sleeper(to, current);
 		hrtimer_set_expires_range_ns(&to->timer, *abs_time,
-					     current->timer_slack_ns);
+					     task_get_effective_timer_slack(current));
 	}
 
 	/*
