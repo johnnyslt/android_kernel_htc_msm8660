6cf25aa187978b0d17110a392d7c689c666146b6
Darescu Ionut <ionut.darescu@gmail.com>
2013-09-07 10:59:41 +0300

    Revert "Add Dynamic read-ahead"
    This reverts commit 3d063ae6e20c4259ab6c2722e9e45196c1bd31bf.

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -1473,9 +1473,7 @@ void task_dirty_inc(struct task_struct *tsk);
 
 /* readahead.c */
 #define VM_MAX_READAHEAD	2048	/* kbytes */
-#define VM_MIN_READAHEAD	64	/* kbytes (includes current page) */
-
-extern unsigned long max_readahead_pages;
+#define VM_MIN_READAHEAD	16	/* kbytes (includes current page) */
 
 int force_page_cache_readahead(struct address_space *mapping, struct file *filp,
 			pgoff_t offset, unsigned long nr_to_read);
